45d95e8c31446ec860d484931aef8c9559e81586
CosmonautJones <travisj_jones@hotmail.com>
2018-03-22 12:32:37 -0500

    init - setting up a simple server

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const cors = require('cors');
+const morgan = require('morgan');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const server = express();
+
+const routes = require('./api/routes/routes');
+
+mongoose.Promise = global.Promise;
+// mongoose.connect('localhost:3000', { useMongoClient: true });
+
+server.use(morgan('dev'));
+server.use(bodyParser.json());
+
+
+routes(server);
+
+const port = process.env.PORT || 3031;
+
+server.listen(port, () => {
+  console.log(`Server up and running on ${port}`);
+});
