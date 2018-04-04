1892c81bb013e1c573984726797f49fd2ed77c33
David Long <davdaarn@live.com>
2018-04-04 11:24:44 -0500

    convo

--- backend/api/controllers/messageController.js
+++ backend/api/controllers/messageController.js
@@ -25,17 +25,18 @@ const sendMessage = (req, res) => {
   });
 };
 
-const questions = [
-  'how are you?',
-  'are you special?',
-  'is your life all that you expected?',
-];
+const questions = [];
 
 const answers = [];
 
 const receiveMessage = async (req, res) => {
-  // rtm.start();
-  // // rtm.subscribePresence('U9TKS1XJN');
+  rtm.start();
+  // rtm.subscribePresence('U9TKS1XJN');
+  const convo = await Account.findById({
+    _id: '5ac43b3b30f8f724b88b4e90',
+  });
+  questionsss = convo.conversations[0].questions;
+  console.log(questionsss);
 
   // const conversationId = 'C7YJ65J10';
   // const queLength = questions.length;
@@ -44,6 +45,7 @@ const receiveMessage = async (req, res) => {
   //   .sendMessage(questions[currentQuestion], conversationId)
   //   .then(res => {
   //     console.log('Message sent: ', res);
+  //     questions;
   //     currentQuestion++;
   //   })
   //   .catch(console.error);
@@ -66,72 +68,27 @@ const receiveMessage = async (req, res) => {
   //     //     channel: conversationId,
   //   }
   // });
-  const newConversation = new Conversation({
-    // title,
-    // questions,
-    // participants,
-    // schedule,
-  });
+
+  const newConversation = new Conversation({});
   const newResponse = new Response({
-    name: 'test',
+    user: 'test',
+    questions,
+    answers,
   });
+
   const test = await Account.findById({
     _id: '5ac43b3b30f8f724b88b4e90',
   });
 
-  // console.log(test);
   await test.conversations.push(newConversation);
   await test.conversations.remove(newConversation);
-  await test.conversations[0].responses.push(newResponse);
-
-  // console.log(test.conversations[0].responses);
-
-  await test.save(function(err, model, numAffected) {
-    if (err) return handleError(err);
-    // console.log(typeof model.conversations[0].responses);
-
-    console.log(numAffected, 'Success!');
-    // console.log(model);
+  await test.conversations[0].responses.push({
+    submittedOn: Date.now,
+    user: 'String',
+    avatar: 'String',
+    questions: [],
+    answers: [],
   });
-
-  // const test = await Account.findById(
-  //   {
-  //     _id: '5ac43b3b30f8f724b88b4e90',
-  //   },
-  //   function(err, model) {
-  //     model.conversations[0].responses.push(newResponse);
-  //     console.log('err', err);
-  //     console.log('model', model.conversations[0].responses);
-  //     model.save();
-  //   }
-  // );
-
-  // console.log(test);
-
-  // Account.findById('5ac4043adb793939184b5eee', async function(err, model) {
-  //   await model.conversations.forEach(c => {
-  //     // console.log(c);
-  //     // console.log(typeof c._id);
-  //     // 'Account.conversations[0].participants[0].responces';
-  //     // console.log(c._id.toString() === '5ac40502de0d4346ecc923d5');
-  //     const response = { user: 'ham dawg' };
-  //     // if (c._id.toString() === '5ac4119e5c6e9628645c5c34') {
-  //     console.log('yes');
-  //     Account.findByIdAndUpdate(
-  //       {
-  //         _id: '5ac4043adb793939184b5eee',
-  //         // 'conversations.3': '5ac4119e5c6e9628645c5c34',
-  //       },
-  //       { $push: { 'conversations.3.title': 'response' } },
-  //       { safe: true, upsert: true, new: true },
-  //       function(err, model) {
-  //         console.log('err', err);
-  //         console.log('model', model);
-  //       }
-  //     );
-  //     // }
-  //   });
-  // });
 
   res.status(200).send(test);
 };
