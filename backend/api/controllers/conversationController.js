41cd599b593ae770fb29913c20600f26c01bb83a
David Long <davdaarn@live.com>
2018-04-05 14:04:06 -0500

    conversations

--- backend/api/controllers/conversationController.js
+++ backend/api/controllers/conversationController.js
@@ -4,16 +4,13 @@ const Member = require('../models/memberModel');
 
 const colors = require('colors');
 
-const colors = require('colors');
-
 const createConversation = async (req, res) => {
-  const { title, questions, users, schedule } = req.body;
+  const { title, questions, users, schedule, a_id } = req.body;
   const participants = [];
 
-  await Account.findById('5ac43b3b30f8f724b88b4e90', function(err, model) {
-    console.log(colors.yellow(model.team.members[0].name));
+  await Account.findById(a_id, function(err, model) {
     model.team.members.forEach(m => {
-      console.log(colors.yellow(m.name));
+      // console.log(colors.yellow(m.name));
       users.forEach(u => {
         if (m.id === u) {
           participants.push(m);
@@ -31,7 +28,7 @@ const createConversation = async (req, res) => {
   });
   // console.log(colors.cyan(newConversation));
   await Account.findByIdAndUpdate(
-    '5ac43b3b30f8f724b88b4e90',
+    a_id,
     { $push: { conversations: newConversation } },
     { safe: true, upsert: true, new: true },
     function(err, model) {
@@ -40,36 +37,34 @@ const createConversation = async (req, res) => {
     }
   );
 
-  // const test = await Account.findById({
-  //   _id: '5ac43b3b30f8f724b88b4e90',
-  // });
-
-  // // console.log(test);
-  // await test.conversations.push(newConversation);
-
-  // console.log(test.conversations);
-
-  // await test.save(function(err, model, numAffected) {
-  //   if (err) return handleError(err);
-  //   console.log(numAffected, 'Success!');
-  //   // console.log(model);
-  // });
-
   // const account = await Account.findById('5abd7623729a5b2bf4c3a8db');
   // await account.conversations.push(newConversation);
   // await account.save();
 
-  // const c = await Conversation.find({});
-  // console.log(c);
+  // await account.save(function(err, model, numAffected) {
+  //   if (err) return handleError(err);
+  //   console.log(numAffected, 'Success!');
+  // });
 
-  // console.log(account.conversations[0]);
   res.json(newConversation);
 };
 
-const addResponses = async (req, res) => {
-  const { response, user } = req.body;
+const deleteConversation = async (req, res) => {
+  const { a_id, c_id } = req.body;
+  const account = await Account.findById(a_id);
+  account.conversations.forEach(c => {
+    if (c._id.toString() === c_id) {
+      account.conversations.remove(c);
+    }
+  });
+  account.save();
+  res.json('hi');
+};
 
-  await Account.findById('5abeb1d0b2b1772ff0f1d129', function(err, model) {
+const addResponses = async (req, res) => {
+  const { response, user, a_id } = req.body;
+
+  await Account.findById(a_id, function(err, model) {
     model.conversations.forEach(c => {
       c.for;
       users.forEach(u => {
@@ -89,7 +84,7 @@ const addResponses = async (req, res) => {
   });
   console.log(colors.cyan(newConversation));
   await Account.findByIdAndUpdate(
-    '5abeb1d0b2b1772ff0f1d129',
+    a_id,
     { $push: { conversations: newConversation } },
     { safe: true, upsert: true, new: true },
     function(err, model) {
@@ -100,8 +95,80 @@ const addResponses = async (req, res) => {
   res.json(newConversation);
 };
 
-module.exports = {
-  createConversation,
-  addResponses,
+const receiveMessage = async (req, res) => {
+  // const dm = await webhook.im.open({user: ''});
+  rtm.start();
+  const convo = await Account.findById({
+    _id: '5ac570fdfb14b00014687413',
+  });
+
+  // convo.conversations.forEach(c => {
+  // if (schedule) {}
+  //   c.participants.forEach(p => {});
+  // });
+  // rtm.subscribePresence('U9TKS1XJN');
+  convo.conversations.forEach(c => {
+    console.log(c._id);
+    if (c._id.toString() === '5ac570fdfb14b00014687413') {
+      questions = c.questions;
+    }
+  });
+
+  const conversationId = 'C7YJ65J10';
+  const queLength = questions.length;
+  let currentQuestion = 0;
+  rtm
+    .sendMessage(questions[currentQuestion], conversationId)
+    .then(res => {
+      // console.log('Message sent: ', res);
+      currentQuestion++;
+    })
+    .catch(console.error);
+
+  rtm.on('message', event => {
+    // console.log('event', event);
+    answers.push(event.text);
+    if (currentQuestion < queLength) {
+      rtm
+        .sendMessage(questions[currentQuestion], conversationId)
+        .then(res => {
+          // console.log('Message sent: ', res);
+          currentQuestion++;
+        })
+        .catch(console.error);
+    } else {
+      rtm.disconnect();
+      // web.chat
+      //   .postMessage({
+      //     channel: conversationId,
+      const newConversation = new Conversation({});
+      const newResponse = new Response({
+        user: 'test',
+        questions,
+        answers,
+      });
+
+      convo.conversations.push(newConversation);
+      convo.conversations.remove(newConversation);
+      convo.conversations[0].responses.push({
+        submittedOn: new Date(),
+        user: 'String',
+        avatar: 'String',
+        questions,
+        answers,
+      });
+
+      convo.save();
+
+      console.log(convo.conversations[0]);
+    }
+  });
+
+  res.status(200).send(convo);
 };
 
+module.exports = {
+  createConversation,
+  deleteConversation,
+  addResponses,
+};
