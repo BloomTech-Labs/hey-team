5658dee7757023b231062f492c974aa49e9938bb
David Long <davdaarn@live.com>
2018-04-14 13:44:55 -0500

    init

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
 const cors = require('cors');
 const morgan = require('morgan');
+// const dotenv = require('dotenv');
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+console.log(process.env.DB_HOST);
 const server = express();
 
 const routes = require('./api/routes');
 
 mongoose.Promise = global.Promise;
 // process.env.MONGO
-mongoose.connect('mongodb://localhost:27017/YoTeam', {
-  useMongoClient: true,
-});
+mongoose.connect(process.env.DB_HOST);
 
 server.use(morgan('dev'));
 server.use(bodyParser.json());
