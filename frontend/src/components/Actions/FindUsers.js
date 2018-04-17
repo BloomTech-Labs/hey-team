d2bc0654eeda7120e6f829149e31ee212f5d9500
Alex Cassell <Alex@simulism.net>
2018-04-17 11:11:20 -0400

    No real changes just clearling psuedo additions out

--- frontend/src/components/Actions/FindUsers.js
+++ frontend/src/components/Actions/FindUsers.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const w_id = localStorage.getItem('doc_id');
 export const findUsers = async searchTerm => {
     try {
-        const res = await axios.post('https://6ba3a0d4.ngrok.io/users/all', {
+        const res = await axios.post('https://8df62460.ngrok.io/users/all', {
         w_id: '5ad4cebecb2cb341f09211ee',
         searchTerm,
         });
