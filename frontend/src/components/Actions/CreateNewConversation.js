6b2fdea41926e7c2573a2317a3a7c0c17ecc6cf2
Alex Cassell <Alex@simulism.net>
2018-04-17 19:54:24 -0400

    main object array is filled from web
    The main object is now filled with conversation data from the server. This fills in the view and edit components when clicked.  With a few exceptions.  I will finish changing over my variables and connecting everything in the morning.

---
+++ frontend/src/components/Actions/CreateNewConversation.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+// const w_id = localStorage.getItem('doc_id');
+// export const createNewConversation = async conversation => {
+//     try {
+//         const res = await axios.post('https://4f2abc0c.ngrok.io/conversation/create', {
+//         w_id: '5ad4cebecb2cb341f09211ee',
+//         conversation,
+//         })  .then(function (response) {
+//             console.log(response);
+//         })
+//           .catch(function (error) {
+//             console.log(error);
+//         });
+//     }
+// };
+
--- frontend/src/components/Actions/GetAllConversations.js
+++ frontend/src/components/Actions/GetAllConversations.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 
 const w_id = localStorage.getItem('doc_id');
-export const findUsers = async searchTerm => {
+export const getAllConversations = async conversations => {
     try {
-        const res = await axios.post('https://4f2abc0c.ngrok.io/users/all', {
+        const res = await axios.post('https://4f2abc0c.ngrok.io/conversation/all', {
         w_id: '5ad4cebecb2cb341f09211ee',
-        searchTerm,
+        conversations,
         });
         return res;
     } catch (error) {}
